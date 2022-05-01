71bedd577aad92ad28d63a35b57e2f039f66f60a
cplkimth <cplkimth@naver.com>
2022-05-01 17:31:52 +0900

    1.0.1

---
+++ Client/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace Client;
+
+public record Person(int Age, string Name)
+{
+    public Person(int age) : this(age, "")
+    {
+    }
+
+    public void Deconstruct(out int age, out string name)
+    {
+        age = Age;
+        name = Name;
+    }
+};
--- Client2/Program.cs
+++ Client2/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Client;
+using Client2;
 
 Range range = 1 .. ^1;
 Console.WriteLine("abc"[range]);
